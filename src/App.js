42a7fcdd68d1d81e6c4f5441041e9335a9336653
Shahnawaz <shahnawazkhan333@live.com>
2024-02-12 13:50:21 +0500

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// App.js
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import CreatePost from './components/CreatePost';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="app">
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/create">Create Post</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/create" element={<CreatePost />} />
+					<Route path="/" element={<Home />} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
